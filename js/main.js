894c82acd224397cde048c1d08f9f56efd71d0ed
shan31 <shanmugam.gow@gmail.com>
2016-09-20 10:43:19 +0530

    Header Changes

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ $(".navbar .navbar-header ul li").hover(function()
   }, function() {
       $(this).children(".sub_menu").stop(true,false).slideToggle(500);
   });
+
+$( '.hamburger_icon' ).click(function() {
+  $( "#toggle" ).toggle( "slide" );
+});
